e7d05efbc4112d8711e20702860ed748e13023eb
JKaszczynski <j.kaszczynski@gmail.com>
2018-04-15 19:30:54 +0200

    Process management and screenshot module

---
+++ Snak/ProcessManagement.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Antycheat
+{
+    class ProcessManagement
+    {
+        /// <summary>
+        /// Returns array containing all of running processes
+        /// </summary>
+        /// <returns>Array of running processes</returns>
+        public Process[] getProcesses()
+        {
+            Process[] localAll = Process.GetProcesses();
+            return localAll;
+        }
+
+        /// <summary>
+        /// Checks if prohibited process is running, but is not performing any action.
+        /// </summary>
+        /// <param name="name">Name of process</param>
+        /// <returns>Information whether process is running</returns>
+        public String informAboutUsage(String name)
+        {
+            return checkIfAppIsInUsageAndPerformAction(name,false);
+        }
+
+        /// <summary>
+        /// Checks if prohibited process is running and kills it.
+        /// </summary>
+        /// <param name="name">Name of process</param>
+        /// <returns>Information about performed action</returns>
+        public String informAboutUsageAndKill(String name)
+        {
+            return checkIfAppIsInUsageAndPerformAction(name,true);
+        }
+
+        private String checkIfAppIsInUsageAndPerformAction(String name, Boolean kill)
+        {
+            foreach (Process process in getProcesses())
+            {
+                if (process.ProcessName.Equals(name))
+                {
+                    try
+                    {
+                        showError(process.ProcessName);
+                        if (kill)
+                        {
+                            process.Kill();
+                            return "Zamknieto niedozwolona aplikacje: " + process.ProcessName;
+                        }
+                        return "Wlaczono niedozwolona aplikacje: " + process.ProcessName;
+                    }
+                    catch (Exception e)
+                    {
+                        return "Wystapil blad podczas zamykania aplikacji";
+                    }
+                }
+            }
+            return "Dana aplikacja nie jest otwarta";
+        }
+
+        private void showError(String name)
+        {
+
+            MessageBox.Show(name + " należy do programów zabronionych", "Antycheat",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ Snak/ProhibitedProcesses.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antycheat
+{
+    class ProhibitedProcesses
+    {
+        private List<string> processes;
+
+        public ProhibitedProcesses()
+        {
+            processes = new List<string>();
+        }
+
+        public ProhibitedProcesses(List<string> processesList)
+        {
+            processes = new List<string>();
+        }
+
+        /// <summary>
+        /// Returns a list of prohibited processes
+        /// </summary>
+        /// <returns>List of prohibited processes</returns>
+        public List<string> getProhibitedProcesses()
+        {
+            return processes;
+        }
+
+        /// <summary>
+        /// Checks whether process is prohibited
+        /// </summary>
+        /// <param name="name">Name of process</param>
+        /// <returns>True if process is prohibited, false otherwise</returns>
+        public Boolean isProhibited(string name)
+        {
+            foreach (string processName in processes)
+            {
+                if (name.Equals(processName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Adds a new process to a list of prohibited processes
+        /// </summary>
+        /// <param name="name">Process name</param>
+        public void add(string name)
+        {
+            processes.Add(name);
+        }
+
+        /// <summary>
+        /// Removes process from a list of prohibited processes
+        /// </summary>
+        /// <param name="name">Process name</param>
+        public void remove(string name)
+        {
+            processes.Remove(name);
+        }
+    }
+}
