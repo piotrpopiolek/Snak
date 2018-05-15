376793941090a899017e9dc34e45b27977f6ee94
Piotr <piotr.popiolek93@gmail.com>
2018-05-15 07:43:51 +0200

    Zmiana nazwy aplikacji

--- SNAK/Server Snak/Server Snak/Form1.Designer.cs
+++ SNAK/Server Snak/Server Snak/Form1.Designer.cs
@@ -61,10 +61,9 @@
             // 
             // buttonSend
             // 
-            this.buttonSend.Location = new System.Drawing.Point(11, 185);
-            this.buttonSend.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSend.Location = new System.Drawing.Point(16, 285);
             this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(69, 28);
+            this.buttonSend.Size = new System.Drawing.Size(104, 43);
             this.buttonSend.TabIndex = 2;
             this.buttonSend.Text = "Wyślij";
             this.buttonSend.UseVisualStyleBackColor = true;
@@ -73,11 +72,9 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Location = new System.Drawing.Point(472, 28);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Location = new System.Drawing.Point(708, 43);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(178, 176);
+            this.groupBox1.Size = new System.Drawing.Size(267, 271);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Klienci";
@@ -85,10 +82,10 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(5, 17);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(8, 26);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(171, 160);
+            this.listBox1.Size = new System.Drawing.Size(254, 244);
             this.listBox1.TabIndex = 0;
             // 
             // groupBox2
@@ -97,54 +94,48 @@
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Location = new System.Drawing.Point(472, 208);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Location = new System.Drawing.Point(708, 320);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox2.Size = new System.Drawing.Size(174, 75);
+            this.groupBox2.Size = new System.Drawing.Size(261, 115);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Serwer";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(65, 42);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Location = new System.Drawing.Point(98, 65);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             65535,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(105, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(158, 26);
             this.numericUpDown1.TabIndex = 3;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(65, 17);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Location = new System.Drawing.Point(98, 26);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(106, 20);
+            this.textBox1.Size = new System.Drawing.Size(157, 26);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "127.0.0.1";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 44);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(39, 68);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.Size = new System.Drawing.Size(42, 20);
             this.label3.TabIndex = 1;
             this.label3.Text = "Port:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(5, 17);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(8, 26);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.Size = new System.Drawing.Size(74, 20);
             this.label2.TabIndex = 0;
             this.label2.Text = "Adres IP:";
             // 
@@ -165,31 +156,30 @@
             // listBoxProces
             // 
             this.listBoxProces.FormattingEnabled = true;
-            this.listBoxProces.Location = new System.Drawing.Point(125, 45);
-            this.listBoxProces.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxProces.ItemHeight = 20;
+            this.listBoxProces.Location = new System.Drawing.Point(188, 69);
             this.listBoxProces.Name = "listBoxProces";
             this.listBoxProces.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.listBoxProces.Size = new System.Drawing.Size(137, 160);
+            this.listBoxProces.Size = new System.Drawing.Size(204, 244);
             this.listBoxProces.TabIndex = 7;
             this.listBoxProces.SelectedIndexChanged += new System.EventHandler(this.listBoxProces_SelectedIndexChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(123, 30);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(184, 46);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.Size = new System.Drawing.Size(149, 20);
             this.label4.TabIndex = 8;
             this.label4.Text = "Zabronione procesy";
             // 
             // listBoxConsole
             // 
             this.listBoxConsole.FormattingEnabled = true;
-            this.listBoxConsole.Location = new System.Drawing.Point(11, 220);
-            this.listBoxConsole.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxConsole.ItemHeight = 20;
+            this.listBoxConsole.Location = new System.Drawing.Point(16, 338);
             this.listBoxConsole.Name = "listBoxConsole";
-            this.listBoxConsole.Size = new System.Drawing.Size(443, 56);
+            this.listBoxConsole.Size = new System.Drawing.Size(662, 84);
             this.listBoxConsole.TabIndex = 9;
             // 
             // comboBoxChange
@@ -198,30 +188,27 @@
             this.comboBoxChange.Items.AddRange(new object[] {
             "Usunięcie",
             "Dodanie"});
-            this.comboBoxChange.Location = new System.Drawing.Point(11, 45);
-            this.comboBoxChange.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxChange.Location = new System.Drawing.Point(16, 69);
             this.comboBoxChange.Name = "comboBoxChange";
-            this.comboBoxChange.Size = new System.Drawing.Size(82, 21);
+            this.comboBoxChange.Size = new System.Drawing.Size(121, 28);
             this.comboBoxChange.TabIndex = 11;
             this.comboBoxChange.SelectedIndexChanged += new System.EventHandler(this.comboBoxChange_SelectedIndexChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(8, 30);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(12, 46);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.Size = new System.Drawing.Size(92, 20);
             this.label5.TabIndex = 10;
             this.label5.Text = "Modyfikacje";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 68);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 105);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(81, 20);
             this.label1.TabIndex = 6;
             this.label1.Text = "Tryb pracy";
             // 
@@ -234,57 +221,54 @@
             this.comboBoxMode.Items.AddRange(new object[] {
             "Aktywny",
             "Pasywny"});
-            this.comboBoxMode.Location = new System.Drawing.Point(11, 83);
-            this.comboBoxMode.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxMode.Location = new System.Drawing.Point(16, 128);
             this.comboBoxMode.Name = "comboBoxMode";
-            this.comboBoxMode.Size = new System.Drawing.Size(82, 21);
+            this.comboBoxMode.Size = new System.Drawing.Size(121, 28);
             this.comboBoxMode.TabIndex = 5;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(295, 30);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(442, 46);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.Size = new System.Drawing.Size(150, 20);
             this.label6.TabIndex = 12;
             this.label6.Text = "Zabronione domeny";
             // 
             // listBoxDomena
             // 
             this.listBoxDomena.FormattingEnabled = true;
-            this.listBoxDomena.Location = new System.Drawing.Point(298, 45);
-            this.listBoxDomena.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxDomena.ItemHeight = 20;
+            this.listBoxDomena.Location = new System.Drawing.Point(447, 69);
             this.listBoxDomena.Name = "listBoxDomena";
             this.listBoxDomena.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.listBoxDomena.Size = new System.Drawing.Size(137, 160);
+            this.listBoxDomena.Size = new System.Drawing.Size(204, 244);
             this.listBoxDomena.TabIndex = 13;
             this.listBoxDomena.SelectedIndexChanged += new System.EventHandler(this.listBoxDomena_SelectedIndexChanged);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(8, 144);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(12, 222);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.Size = new System.Drawing.Size(57, 20);
             this.label7.TabIndex = 14;
             this.label7.Text = "Nazwa";
             // 
             // textBoxNazwa
             // 
-            this.textBoxNazwa.Location = new System.Drawing.Point(11, 160);
+            this.textBoxNazwa.Location = new System.Drawing.Point(16, 246);
+            this.textBoxNazwa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxNazwa.Name = "textBoxNazwa";
-            this.textBoxNazwa.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNazwa.Size = new System.Drawing.Size(148, 26);
             this.textBoxNazwa.TabIndex = 15;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(8, 106);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(12, 163);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.Size = new System.Drawing.Size(59, 20);
             this.label8.TabIndex = 16;
             this.label8.Text = "Rodzaj";
             // 
@@ -297,17 +281,16 @@
             this.comboBoxRodzaj.Items.AddRange(new object[] {
             "Proces",
             "Domena"});
-            this.comboBoxRodzaj.Location = new System.Drawing.Point(11, 121);
-            this.comboBoxRodzaj.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxRodzaj.Location = new System.Drawing.Point(16, 186);
             this.comboBoxRodzaj.Name = "comboBoxRodzaj";
-            this.comboBoxRodzaj.Size = new System.Drawing.Size(82, 21);
+            this.comboBoxRodzaj.Size = new System.Drawing.Size(121, 28);
             this.comboBoxRodzaj.TabIndex = 17;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 319);
+            this.ClientSize = new System.Drawing.Size(1002, 491);
             this.Controls.Add(this.comboBoxRodzaj);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.textBoxNazwa);
@@ -325,10 +308,9 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.buttonSend);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Serwer SNAK";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
