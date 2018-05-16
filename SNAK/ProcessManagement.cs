24c0f0afb20ec1c2065510e200896f0b46c552c9
MistykQQ <palicki.maciej@wp.pl>
2018-05-17 00:36:19 +0200

    Add files via upload

---
+++ SNAK/ProcessManagement.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+
+namespace Antycheat
+{
+    class ProcessManagement
+    {
+
+        /* private Socket clientSocket;
+
+         public ProcessManagement(Socket clientSocket)
+         {
+             this.clientSocket = clientSocket;
+         }*/
+
+        /// <summary>
+        /// Guards client and inform server about illegal actions (works in infinite loop)
+        /// </summary>
+        public void guardAggressive()
+        {
+            while (true)
+            {
+                List<String> prohibited = ProhibitedProcesses.getProhibitedProcesses();
+                for(int i = 0; i < prohibited.Count; i++)
+                {
+                    Process[] listOfProcesses = Process.GetProcessesByName(prohibited[i]);
+                    for(int j = 0; j < listOfProcesses.Length; j++)
+                    {
+                        try { 
+                        listOfProcesses[j].Kill();
+                        // tutaj poinformuj serwer o zlamaniu zakazu
+                        ThreadStart childref = new ThreadStart(showError);
+                        Thread childThread = new Thread(childref);
+                        childThread.Start();
+                        }catch(Exception e)
+                        {
+                            Debug.WriteLine(e.ToString());
+                        }
+                     }
+                }
+                Thread.Sleep(3000);
+            }
+        }
+
+        /// <summary>
+        ///  Informs server about illegal actions (works in infinite loop)
+        /// </summary>
+        public void guardPassive()
+        {
+            while (true)
+            {
+                List<String> prohibited = ProhibitedProcesses.getProhibitedProcesses();
+                for (int i = 0; i < prohibited.Count; i++)
+                {
+                    Process[] listOfProcesses = Process.GetProcessesByName(prohibited[i]);
+                    for (int j = 0; j < listOfProcesses.Length; j++)
+                    {
+                        // tutaj poinformuj serwer o zlamaniu zakazu
+                    }
+                }
+                Thread.Sleep(3000);
+            }
+        }
+
+        /// <summary>
+        /// Returns array containing all of running processes
+        /// </summary>
+        /// <returns>Array of running processes</returns>
+        public Process[] getProcesses()
+        {
+            Process[] localAll = Process.GetProcesses();
+            return localAll;
+        }
+
+        private void showError()
+        {
+            MessageBox.Show("Program należy do programów zabronionych", "Antycheat",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ SNAK/Server Snak/Server Snak/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace Server_Snak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.listBoxProces = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBoxConsole = new System.Windows.Forms.ListBox();
+            this.comboBoxChange = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxMode = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBoxDomena = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxNazwa = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBoxRodzaj = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(11, 185);
+            this.buttonSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(69, 28);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Wyślij";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(472, 28);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(178, 212);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Klienci";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(5, 17);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(171, 160);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(472, 244);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(174, 64);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Serwer";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(65, 42);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(105, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 17);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 44);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Adres IP:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // listBoxProces
+            // 
+            this.listBoxProces.FormattingEnabled = true;
+            this.listBoxProces.Location = new System.Drawing.Point(125, 45);
+            this.listBoxProces.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBoxProces.Name = "listBoxProces";
+            this.listBoxProces.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBoxProces.Size = new System.Drawing.Size(137, 160);
+            this.listBoxProces.TabIndex = 7;
+            this.listBoxProces.SelectedIndexChanged += new System.EventHandler(this.listBoxProces_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 30);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Zabronione procesy";
+            // 
+            // listBoxConsole
+            // 
+            this.listBoxConsole.FormattingEnabled = true;
+            this.listBoxConsole.Location = new System.Drawing.Point(11, 220);
+            this.listBoxConsole.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBoxConsole.Name = "listBoxConsole";
+            this.listBoxConsole.Size = new System.Drawing.Size(443, 56);
+            this.listBoxConsole.TabIndex = 9;
+            // 
+            // comboBoxChange
+            // 
+            this.comboBoxChange.FormattingEnabled = true;
+            this.comboBoxChange.Items.AddRange(new object[] {
+            "Usunięcie",
+            "Dodanie"});
+            this.comboBoxChange.Location = new System.Drawing.Point(11, 45);
+            this.comboBoxChange.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxChange.Name = "comboBoxChange";
+            this.comboBoxChange.Size = new System.Drawing.Size(82, 21);
+            this.comboBoxChange.TabIndex = 11;
+            this.comboBoxChange.SelectedIndexChanged += new System.EventHandler(this.comboBoxChange_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 30);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Modyfikacje";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tryb pracy";
+            // 
+            // comboBoxMode
+            // 
+            this.comboBoxMode.AutoCompleteCustomSource.AddRange(new string[] {
+            "Aktywny",
+            "Pasywny"});
+            this.comboBoxMode.FormattingEnabled = true;
+            this.comboBoxMode.Items.AddRange(new object[] {
+            "Aktywny",
+            "Pasywny"});
+            this.comboBoxMode.Location = new System.Drawing.Point(11, 83);
+            this.comboBoxMode.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxMode.Name = "comboBoxMode";
+            this.comboBoxMode.Size = new System.Drawing.Size(82, 21);
+            this.comboBoxMode.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(295, 30);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Zabronione domeny";
+            // 
+            // listBoxDomena
+            // 
+            this.listBoxDomena.FormattingEnabled = true;
+            this.listBoxDomena.Location = new System.Drawing.Point(298, 45);
+            this.listBoxDomena.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBoxDomena.Name = "listBoxDomena";
+            this.listBoxDomena.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBoxDomena.Size = new System.Drawing.Size(137, 160);
+            this.listBoxDomena.TabIndex = 13;
+            this.listBoxDomena.SelectedIndexChanged += new System.EventHandler(this.listBoxDomena_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 144);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Nazwa";
+            // 
+            // textBoxNazwa
+            // 
+            this.textBoxNazwa.Location = new System.Drawing.Point(11, 160);
+            this.textBoxNazwa.Name = "textBoxNazwa";
+            this.textBoxNazwa.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNazwa.TabIndex = 15;
+            this.textBoxNazwa.TextChanged += new System.EventHandler(this.textBoxNazwa_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 106);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Rodzaj";
+            // 
+            // comboBoxRodzaj
+            // 
+            this.comboBoxRodzaj.AutoCompleteCustomSource.AddRange(new string[] {
+            "Aktywny",
+            "Pasywny"});
+            this.comboBoxRodzaj.FormattingEnabled = true;
+            this.comboBoxRodzaj.Items.AddRange(new object[] {
+            "Proces",
+            "Domena"});
+            this.comboBoxRodzaj.Location = new System.Drawing.Point(11, 121);
+            this.comboBoxRodzaj.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxRodzaj.Name = "comboBoxRodzaj";
+            this.comboBoxRodzaj.Size = new System.Drawing.Size(82, 21);
+            this.comboBoxRodzaj.TabIndex = 17;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(8, 182);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(122, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Wyslij do wszystkich";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 319);
+            this.Controls.Add(this.comboBoxRodzaj);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxNazwa);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBoxDomena);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBoxChange);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBoxConsole);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBoxProces);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxMode);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Serwer SNAK";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.ListBox listBoxProces;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBoxConsole;
+        private System.Windows.Forms.ComboBox comboBoxChange;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxMode;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBoxDomena;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxNazwa;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBoxRodzaj;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
